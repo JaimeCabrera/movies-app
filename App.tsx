83d80e83e5afa81076618399f8fb602d44935904
Jaime Cabrera <jycabrerap@unl.edu.ec>
2022-03-17 12:02:33 -0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+export default App;
